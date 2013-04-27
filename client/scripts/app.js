9457c6453c49f0b4512b20f4d5f4bd7539b77bd9
Jukka-Pekka Louneva <jukka-pekka.louneva@siili.fi>
2013-04-27 15:24:51 +0300

    Disable hover states on touch devises.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,8 @@
 window.App = angular.module('Shoppinglist', ['ngResource']);
+
+window.onload = function() {
+	if ('ontouchstart' in document) {
+		var body = document.getElementsByTagName('body')[0];
+		body.className = body.className + ' touch';
+	}
+};
