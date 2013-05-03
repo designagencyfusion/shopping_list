3c53aafebb9eb33cecf97d0ad2be3b1958533210
Heikki Ylönen <heikki74@gmail.com>
2013-05-03 17:11:52 +0300

    Muista aiemmin syötetyt tuotteet (autocomplete). Fixes #6

--- client/scripts/directives.js
+++ client/scripts/directives.js
@@ -5,6 +5,28 @@ App.directive('autocomplete', [
 			restrict: 'A',
 			link: function(scope, element, attrs) {
 
+				element.autocomplete({
+					select: function() {
+						$timeout(function() {
+							element.trigger('input');
+						});
+					}
+				});
+
+				scope.$watch('items', function() {
+					var names = scope.items.map(function(item) {
+						return item.title;
+					});
+					names = names.filter(function(el, index, arr) {
+						return index == arr.indexOf(el);
+					});
+					element.autocomplete('option', 'source', names);
+				}, true);
+
+				element.on('$destroy', function() {
+					element.autocomplete('destroy');
+				});
+
 			}
 		};
 	}
