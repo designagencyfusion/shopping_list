7710262ca68e4cd93811b2f77c96014d3c397681
Heikki Ylönen <heikki74@gmail.com>
2013-05-03 15:54:15 +0300

    Lokalisoi jollain tapaa. Fixes #5

--- client/scripts/filters.js
+++ client/scripts/filters.js
@@ -2,8 +2,39 @@ App.filter('i18n', [
 	'$locale',
 	function($locale) {
 
-		return function(key) {
-			return key;
+		var translations = {
+			'en-us': {
+				'shoppingList': {
+					'hint': 'Write in form "tomatoes 500g"...',
+					'hideBoughtItems': 'Hide bought items',
+					'showAllItems': 'Show all items'
+				}
+			},
+			'fi-fi': {
+				'shoppingList': {
+					'hint': 'Lisää muodossa "tomaatteja 500g"...',
+					'hideBoughtItems': 'Piilota ostetut',
+					'showAllItems': 'Näytä kaikki'
+				}
+			}
+		};
+
+		function findByKeyStr(keyStr) {
+			var keys = keyStr.split('.');
+			var match = translations[$locale.id];
+			for (var i = 0; i < keys.length; i++) {
+				if (match.hasOwnProperty(keys[i])) {
+					match = match[keys[i]];
+				} else {
+					match = null;
+					break;
+				}
+			}
+			return match;
+		}
+
+		return function(key, replacement) {
+			return findByKeyStr(key);
 		};
 
 	}
