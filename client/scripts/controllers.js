041ba3ea51a8a78fe781715a0d2bb611f7bfd5e4
Jukka-Pekka Louneva <jukka-pekka.louneva@siili.fi>
2014-11-19 00:05:08 +0200

    Suggestions (results for now) as a separate directive.

--- client/scripts/controllers.js
+++ client/scripts/controllers.js
@@ -49,58 +49,10 @@ App.controller('ShoppingListCtrl',
 		$scope.items = Item.query({ listId: $routeParams.id, archived: false });
 		$scope.home = '#/shopping-lists/' + $routeParams.id;
 
-		var defaultUnit = '';
-		$scope.$locale = $locale;
-		$scope.$watch('$locale.id', function() {
-			defaultUnit = $filter('i18n')('units.pcs');
-		});
-
 		$scope.newItem = new Item({
 			string: '',
 			bought: false
 		});
-
-		$scope.formItem = function() {
-			var match, matches;
-			var string = $scope.newItem.string || '';
-			$scope.newItem.reverse = false;
-
-			// First try "tomatoes 500g" ...
-			match = string.match(/ ([0-9]*)(\s?)([a-zA-Z]*?)$/);
-			if (match) {
-				matches = {
-					amount: match[1],
-					unit: match[3],
-					replace: match[0],
-					title: string.replace(match[0], '')
-				};
-			}
-			// ... and if that doesn't match, try "500g tomatoes".
-			if (!matches || !matches.amount) {
-				match = string.match(/^([0-9]*)(\s?)([a-zA-Z]*?) /);
-				if (match) {
-					matches = {
-						amount: match[1] !== '' ? match[1] : null,
-						unit: match[3],
-						replace: match[0],
-						title: string.replace(match[0], '')
-					};
-					$scope.newItem.reverse = true;
-				}
-			}
-
-			if (match && match.length > 1 && (matches && matches.amount)) {
-				// Pick title, amound and unit if available ...
-				$scope.newItem.title = matches.title;
-				$scope.newItem.amount = parseInt(matches.amount, 10);
-				$scope.newItem.unit = matches.unit || defaultUnit;
-			} else {
-				// ... and if not add the whole string to the title.
-				$scope.newItem.title = string;
-				$scope.newItem.amount = null;
-				$scope.newItem.unit = null;
-			}
-		};
 
 		$scope.createItem = function() {
 
--- client/scripts/directives.js
+++ client/scripts/directives.js
@@ -46,3 +46,67 @@ App.directive('siiListItem',
 		};
 	}
 );
+
+App.directive('siiSuggestions',
+	function($locale, $filter) {
+		return {
+			restrict: 'E',
+			templateUrl: '/templates/suggestions.html',
+			replace: true,
+			scope: {
+				item: '=',
+				locale: '='
+			},
+			link: function(scope) {
+
+				// Suggestion engine
+				function suggest() {
+					var defaultUnit = $filter('i18n')('units.pcs');
+					var match, matches;
+					var string = scope.item.string || '';
+					scope.item.reverse = false;
+
+					// First try "tomatoes 500g" ...
+					match = string.match(/ ([0-9]*)(\s?)([a-zA-Z]*?)$/);
+					if (match) {
+						matches = {
+							amount: match[1],
+							unit: match[3],
+							replace: match[0],
+							title: string.replace(match[0], '')
+						};
+					}
+					// ... and if that doesn't match, try "500g tomatoes".
+					if (!matches || !matches.amount) {
+						match = string.match(/^([0-9]*)(\s?)([a-zA-Z]*?) /);
+						if (match) {
+							matches = {
+								amount: match[1] !== '' ? match[1] : null,
+								unit: match[3],
+								replace: match[0],
+								title: string.replace(match[0], '')
+							};
+							scope.item.reverse = true;
+						}
+					}
+
+					if (match && match.length > 1 && (matches && matches.amount)) {
+						// Pick title, amound and unit if available ...
+						scope.item.title = matches.title;
+						scope.item.amount = parseInt(matches.amount, 10);
+						scope.item.unit = matches.unit || defaultUnit;
+					} else {
+						// ... and if not add the whole string to the title.
+						scope.item.title = string;
+						scope.item.amount = null;
+						scope.item.unit = null;
+					}
+				};
+
+				// Watch for changes in locale id
+				scope.$locale = $locale;
+				scope.$watch('[$locale.id, item.string] | json', suggest);
+			}
+		};
+	}
+);
